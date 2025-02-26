ea634031344048080061853b5e573ad2ce268b9c
Rajeev kumar <krrajeev0000000000@gmail.com>
2025-02-26 21:14:34 +0530

    Setup.py file and other venv files

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+def get_requirements()->List[str]:
+    '''
+    This function will return list of requirements
+    '''
+    
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #read lines from file
+            lines=file.readlines()
+            
+            #process each line
+            for line in lines:
+                requirement = line.strip()
+                #ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+                    
+    
+    except FileNotFoundError:
+        print('requirements.txt file not found!')
+    
+    return requirement_lst
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Rajeev kumar",
+    author_email="krrajeev0000000000@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
